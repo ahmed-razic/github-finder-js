f0429c665290391cf5dc98d5240543394e7ce692
ahmed-razic <ahmed.razic@gmail.com>
2022-04-06 22:56:41 +0200

    ui

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 const searchUser = document.getElementById('searchUser');
 searchUser.addEventListener('keyup', (e) => {
   const userText = e.target.value;
+  const github = new GitHub();
+  const ui = new UI();
 
   if (userText !== '') {
-    const github = new GitHub();
     github.getUser(userText).then((data) => {
       if (data.profile.message === 'Not Found') {
         console.log('User not found');
       } else {
-        console.log(data);
+        ui.showProfile(data.profile);
       }
     });
   } else {
     //clear profile
+    ui.clearProfile();
   }
 });
