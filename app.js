3b4f7c1d4731977be714b5dbe510b440bbcd6e33
ahmed-razic <ahmed.razic@gmail.com>
2022-04-06 23:17:22 +0200

    added repos

--- app.js
+++ app.js
@@ -7,9 +7,10 @@ searchUser.addEventListener('keyup', (e) => {
   if (userText !== '') {
     github.getUser(userText).then((data) => {
       if (data.profile.message === 'Not Found') {
-        console.log('User not found');
+        ui.showAlert('User not found', 'alert alert-danger');
       } else {
         ui.showProfile(data.profile);
+        ui.showRepos(data.repos);
       }
     });
   } else {
--- github.js
+++ github.js
@@ -2,6 +2,8 @@ class GitHub {
   constructor() {
     this.client_id = '6db23869cb96ecf6407a';
     this.client_secret = '4e3e2751c86dc8c1c22bbcc5fc60f1df87fbf61a';
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc';
   }
 
   async getUser(user) {
@@ -9,8 +11,13 @@ class GitHub {
       `https://api.github.com/users/${user}?client_id=${this.client_id}&this.client_secret=${this.client_secret}`
     );
 
-    const profile = await profileResponse.json();
+    const reposResponse = await fetch(
+      `https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&this.client_secret=${this.client_secret}`
+    );
 
-    return { profile: profile };
+    const profile = await profileResponse.json();
+    const repos = await reposResponse.json();
+
+    return { profile: profile, repos: repos };
   }
 }
